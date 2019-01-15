aed574077773132eb7f0117b8f6b814ca39ce15d
nasriime <mnasr4040@gmail.com>
2019-01-15 23:40:03 +0200

    adding two filters to reviews component install lodash change gallary to gallery component

---
+++ src/components/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img v-for="(image,index) in images" :key="index" :src="image.photo" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gallery',
+  props: {
+    images: Array,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
-
+    <ul>
+      <li v-for="(item, index) in filteredReviews" :key="index">
+        {{ item.review }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'Reviews',
-
+  computed: {
+    orderedReviews() {
+      return _.orderBy(this.reviews, 'score');
+    },
+    filteredReviews() {
+      return this.orderedReviews().slice(0, 3);
+    },
+  },
 };
 </script>
 
