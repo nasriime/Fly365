a625c9091a6169448bddcfe5d1ad1695229f1c74
nasriime <mnasr4040@gmail.com>
2019-01-18 16:48:57 +0200

    adding pagination

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -12,9 +12,16 @@
           {{ item.score }}
         </li>
       </ul>
+
+       <Pagination
+      :total-pages="totalPages"
+      :total="reviews.length"
+      :per-page="3"
+      :current-page="currentPage"
+      @pagechanged="onPageChange"/>
     </div>
 
-    <Pagination />
+
   </div>
 </template>
 
@@ -27,6 +34,7 @@ export default {
   data() {
     return {
       asc: false,
+      currentPage: 1,
     };
   },
   components: {
@@ -39,6 +47,9 @@ export default {
     orderedReviews() {
       return _.orderBy(this.reviews, 'score', this.asc ? 'asc' : 'desc');
     },
+    totalPages() {
+      return Math.ceil(this.reviews.length / 3);
+    },
     // filteredReviews() {
     //   return this.orderedReviews().slice(0, 3);
     // },
@@ -47,10 +58,25 @@ export default {
     toggleSort() {
       this.asc = !this.asc;
     },
+    onPageChange(page) {
+      console.log(page);
+      this.currentPage = page;
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
+.pagination {
+  list-style-type: none;
+}
 
+.pagination-item {
+  display: inline-block;
+}
+
+.active {
+  background-color: #4AAE9B;
+  color: #ffffff;
+}
 </style>
